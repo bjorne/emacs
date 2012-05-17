be65483f4f38320c0a872e853a1a64c7259dddc9
Bjorn <bjorne@markov.(none)>
2012-05-17 23:44:40 +0200

    Fix broken init.

--- init.el
+++ init.el
@@ -18,8 +18,11 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (unless (require 'el-get nil t)
   (with-current-buffer
-      (url-retrieve-synchronously "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (lambda (s) (let (el-get-master-branch) (end-of-buffer) (eval-print-last-sexp)))))
+      (url-retrieve "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+		    (lambda (s)
+		      (let (el-get-master-branch)
+			(end-of-buffer)
+			(eval-print-last-sexp))))))
 
 (setq el-get-sources '((:name idle-highlight-mode
                               :type github
