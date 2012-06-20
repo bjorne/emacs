a1922016c900698c8200df784e6279d04ab93caf
Björn Ramberg <bjorn@burtcorp.com>
2012-06-20 13:43:40 +0200

    js repl config.

--- bjorne-misc.el
+++ bjorne-misc.el
@@ -161,4 +161,17 @@
 ;; coffee tab width 2
 (setq coffee-tab-width 2)
 
+;; js repl
+(setq inferior-js-program-command "node")
+
+(setq inferior-js-mode-hook
+      (lambda ()
+        ;; We like nice colors
+        (ansi-color-for-comint-mode-on)
+        ;; Deal with some prompt nonsense
+        (add-to-list 'comint-preoutput-filter-functions
+                     (lambda (output)
+                       (replace-regexp-in-string ".*1G\.\.\..*5G" "..."
+                                                 (replace-regexp-in-string ".*1G.*3G" "&gt;" output))))))
+
 (provide 'bjorne-misc)
