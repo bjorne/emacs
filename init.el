4adcc0304c779e52d27dfbc6d6b74cdad51f2e90
Björn Ramberg <bjorn@burtcorp.com>
2012-09-27 22:05:32 +0200

    Make the coffe repl nice.

--- init.el
+++ init.el
@@ -94,6 +94,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(coffee-args-repl (quote ("-i" "NODE_NO_READLINE=1")))
  '(css-indent-offset 2)
  '(js2-basic-offset 2)
  '(js2-mirror-mode t)
