ba10cf032b0707bdeb3c3d1807b39b0e54b2c148
Björn Ramberg <bjorn@burtcorp.com>
2012-09-27 22:06:22 +0200

    Various options.

--- init.el
+++ init.el
@@ -13,6 +13,7 @@
 ;; root dir
 (setq bjorne-root "~/.emacs.d/")
 (add-to-list 'load-path bjorne-root)
+(add-to-list 'load-path (concat bjorne-root "vendor/"))
 
 ;; Turn off mouse interface early in startup to avoid momentary display
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
@@ -96,6 +97,7 @@
  ;; If there is more than one, they won't work right.
  '(coffee-args-repl (quote ("-i" "NODE_NO_READLINE=1")))
  '(css-indent-offset 2)
+ '(ispell-program-name "aspell")
  '(js2-basic-offset 2)
  '(js2-mirror-mode t)
  '(lintnode-port 3000)
@@ -109,10 +111,14 @@
  '(magit-set-upstream-on-push t)
  '(magit-status-buffer-switch-function (quote switch-to-buffer))
  '(safe-global-variable-values (quote ((eval ignore-errors "Write-contents-functions is a buffer-global alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t))))
- '(send-mail-function (quote mailclient-send-it)))
+ '(send-mail-function (quote mailclient-send-it))
+;;  '(yas/fallback-behavior (quote call-other-command))
+ '(yas/prompt-functions (quote (yas/ido-prompt yas/x-prompt yas/dropdown-prompt yas/completing-prompt yas/ido-prompt yas/no-prompt)))
+ '(yas/trigger-key nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(linum ((t (:inherit (shadow default) :inverse-video t :height 0.8)))))
+(put 'narrow-to-region 'disabled nil)
