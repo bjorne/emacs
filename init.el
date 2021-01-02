572f116dc714a442d6f50e9b0ee80b5963f2f897
Björn Ramberg <bjornr@spotify.com>
2021-01-02 11:09:33 +0100

    Package changes

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;; (package-initialize)
 
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 
@@ -36,8 +36,8 @@
 
 
 ;; colors!
-(require 'color-theme-bjorne)
-(color-theme-bjorne)
+; (require 'color-theme-bjorne)
+; (color-theme-bjorne)
 
 (require 'bjorne-defuns)
 (require 'bjorne-misc)
@@ -186,8 +186,11 @@
   :config
   (progn
     (define-key projectile-mode-map (kbd "C-c p t") 'projectile-toggle-test-code)
-    (define-key projectile-mode-map (kbd "C-c p g") 'ag-project)
-    (setq projectile-enable-caching t)
+    ;; (define-key projectile-mode-map (kbd "C-c p g") 'ag-project)
+    ;; (define-key projectile-mode-map (kbd "C-c p f") 'projectile-find-file)
+    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+    (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
+    (setq projectile-enable-caching t) 
     (setq projectile-completion-system 'ivy)
     (setq projectile-cache-file (concat var-dir "projectile.cache"))
     (setq projectile-known-projects-file (concat var-dir "projectile-bookmarks.eld"))
@@ -202,18 +205,18 @@
   :ensure t
   :init (counsel-projectile-mode))
 (use-package powerline
-  :config
-  (progn
-    (set-face-background 'powerline-active1 "gray18")
-    (set-face-background 'powerline-active2 "DarkGreen")
-    (set-face-foreground 'powerline-active1 "white")
-    (set-face-foreground 'powerline-active2 "white")
-    (set-face-background 'mode-line "ForestGreen")
-    (set-face-foreground 'mode-line "white")
-    (set-face-attribute 'mode-line-inactive nil :box nil)
-    (set-face-background 'powerline-inactive1 "gray22")
-    (set-face-background 'powerline-inactive2 "gray11")
-    (powerline-default-theme)))
+   :config
+   (progn
+;;     (set-face-background 'powerline-active1 "gray18")
+     ;; (set-face-background 'powerline-active2 "DarkGreen")
+;;     (set-face-foreground 'powerline-active1 "white")
+;;     (set-face-foreground 'powerline-active2 "white")
+     (set-face-background 'mode-line "ForestGreen")
+;;     (set-face-foreground 'mode-line "white")
+;;     (set-face-attribute 'mode-line-inactive nil :box nil)
+;;     (set-face-background 'powerline-inactive1 "gray22")
+;;     (set-face-background 'powerline-inactive2 "gray11")
+     (powerline-default-theme)))
 (use-package magit
   :init
   (progn
@@ -251,6 +254,8 @@
     (setq reb-re-syntax 'string)))
 (use-package pcre2el
   :ensure t)
+(use-package ess
+  :ensure t)
 (use-package dockerfile-mode
   :ensure t
   :mode (("^Dockerfile" . dockerfile-mode)))
@@ -263,6 +268,22 @@
   (yas-reload-all))
 (use-package yasnippet-snippets
   :ensure t)
+;; (use-package solarized-theme
+;;   :ensure t
+;;   :defer t
+;;   :init (load-theme 'solarized-dark t))
+;; (use-package color-theme-sanityinc-tomorrow
+;;   :ensure t)
+;; ;; (use-package badger-theme
+;; ;;   :ensure t
+;; ;;   :config (load-theme 'badger t))
+;; (use-package alect-themes
+;;   :ensure t
+;;   :defer t
+;;   :init (load-theme 'alect-dark t))
+(use-package darcula-theme
+  :ensure t)
+;;  :init (load-theme 'darcula))
 (use-package tide
   :ensure t
   :config (progn
@@ -276,33 +297,98 @@
                                               (tide-hl-identifier-mode +1)
                                               (company-mode +1)))))
 
+;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+(setq lsp-keymap-prefix "s-l")
+
+(use-package lsp-mode
+  :ensure t
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (scala-mode . lsp)
+         (lsp-mode . lsp-lens-mode)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :config (setq lsp-prefer-flymake nil)
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode :ensure t)
+;; if you are helm user
+(use-package helm-lsp :commands helm-lsp-workspace-symbol :ensure t)
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol :ensure t)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list :ensure t)
+
+;; Use the Debug Adapter Protocol for running tests and debugging
+(use-package posframe
+  ;; Posframe is a pop-up tool that must be manually installed for dap-mode
+  :ensure t
+  )
+;; optionally if you want to use debugger
+(use-package dap-mode
+  :ensure t
+  :hook
+  (lsp-mode . dap-mode)
+  (lsp-mode . dap-ui-mode))
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+;; Enable scala-mode for highlighting, indentation and motion commands
+(use-package scala-mode
+  :ensure t
+  :interpreter
+    ("scala" . scala-mode))
+
+;; Enable sbt mode for executing sbt commands
+(use-package sbt-mode
+  :ensure t
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false")))
+
+;; Add metals backend for lsp-mode
+(use-package lsp-metals
+  :ensure t
+  :config (setq lsp-metals-treeview-show-when-views-received t))
 
 (setq whitespace-style (quote (face tabs empty trailing)))
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '(
-   (sh . t)))
+;; (org-babel-do-load-languages
+;;  'org-babel-load-languages
+;;  '(
+;;    (sh . t)))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ag-highlight-search t)
- '(ag-ignore-list (quote ("builds/" "*.min.js" "dist/")))
- '(coffee-args-repl (quote ("-i" "NODE_NO_READLINE=1")))
+ '(ag-ignore-list '("builds/" "*.min.js" "dist/"))
+ '(coffee-args-repl '("-i" "NODE_NO_READLINE=1"))
  '(css-indent-offset 2)
+ '(custom-safe-themes
+   '("675c0a75d42b42ed963e426b3d8582ec719742b9033179b8d8fef89fae0dfbff" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
  '(ispell-program-name "aspell")
  '(js2-basic-offset 2)
  '(js2-mirror-mode t)
  '(json-reformat:indent-width 2)
  '(lintnode-port 3000)
- '(magit-rebase-arguments (quote ("--autosquash")))
+ '(magit-rebase-arguments '("--autosquash"))
  '(package-selected-packages
-   (quote
-    (scala-mode jq-mode graphql-mode vue-mode esup ansible markdown-preview-mode dash magithub tide company smartparens nvm yasnippet-snippets apib-mode dockerfile-mode pcre2el bundler ruby-tools enh-ruby-mode eslint-fix ggtags avy magit-filenotify browse-at-remote copy-as-format flymd js-auto-beautify rjsx-mode gh-md typescript-mode counsel-projectile lorem-ipsum iedit rbenv ivy-hydra counsel ivy-bibtex flyspell-correct-ivy ivy yasnippet yaml-mode wrap-region web-mode use-package textmate smex scratch rspec-mode restclient projectile prodigy powerline paredit pallet osx-dictionary multiple-cursors markdown-mode mark-multiple magit-gh-pulls json-mode js2-mode js-comint jade-mode imenu-anywhere idle-highlight-mode highlight-indentation haml-mode google-maps gist free-keys flycheck expand-region exec-path-from-shell evm evil drag-stuff discover color-theme coffee-mode ag actionscript-mode ace-jump-mode)))
+   '(lsp-metals sbt-mode which-key dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui lsp-mode ess darcula-theme alect-themes color-theme-sanityinc-tomorrow solarized-theme badger-theme spacemacs-theme scala-mode jq-mode graphql-mode vue-mode esup ansible markdown-preview-mode dash magithub tide company smartparens nvm yasnippet-snippets apib-mode dockerfile-mode pcre2el bundler ruby-tools enh-ruby-mode eslint-fix ggtags avy magit-filenotify browse-at-remote copy-as-format flymd js-auto-beautify rjsx-mode gh-md typescript-mode counsel-projectile lorem-ipsum iedit rbenv ivy-hydra counsel ivy-bibtex flyspell-correct-ivy ivy yasnippet yaml-mode wrap-region web-mode use-package textmate smex scratch rspec-mode restclient projectile prodigy powerline paredit pallet osx-dictionary multiple-cursors markdown-mode mark-multiple magit-gh-pulls json-mode js2-mode js-comint jade-mode imenu-anywhere idle-highlight-mode highlight-indentation haml-mode google-maps gist free-keys flycheck expand-region exec-path-from-shell evm evil drag-stuff discover coffee-mode ag actionscript-mode ace-jump-mode))
  '(rspec-use-bundler-when-possible nil)
- '(send-mail-function (quote mailclient-send-it))
+ '(send-mail-function 'mailclient-send-it)
  '(typescript-indent-level 2)
  '(vue-html-extra-indent 2)
  '(web-mode-code-indent-offset 2)
