e7cb628d7e6bb6f16a1e690c4e11627806543f54
Björn Ramberg <bjornr@spotify.com>
2021-01-04 16:49:24 +0100

    Eshell and vterm conf

--- init.el
+++ init.el
@@ -339,6 +339,41 @@
   ;; :config (setq lsp-metals-treeview-show-when-views-received t)
   )
 
+(defun efs/configure-eshell ()
+  ;; Save command history when commands are entered
+  (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
+
+  ;; Truncate buffer for performance
+  (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
+
+  ;; this doesn't work and I don't know why
+  (define-key eshell-mode-map (kbd "M-r") 'counsel-esh-history)
+
+  (setq eshell-history-size         10000
+        eshell-buffer-maximum-lines 10000
+        eshell-hist-ignoredups t
+        eshell-scroll-to-bottom-on-input t))
+
+(use-package eshell-git-prompt)
+
+(use-package eshell
+  :hook
+  (eshell-first-time-mode . efs/configure-eshell)
+  (eshell-mode . (lambda () (define-key eshell-hist-mode-map (kbd "M-r") 'counsel-esh-history)))
+  :config
+  (with-eval-after-load 'esh-opt
+    (setq eshell-destroy-buffer-when-process-dies t)
+    (setq eshell-visual-commands '("htop" "zsh" "vim")))
+  (eshell-git-prompt-use-theme 'robbyrussell))
+
+(use-package vterm
+  :commands vterm
+  :config
+  (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *")  ;; Set this to match your custom shell prompt
+  ;;(setq vterm-shell "zsh")                       ;; Set this to customize the shell to launch
+  (setq vterm-max-scrollback 10000)
+  (setq vterm-buffer-name-string "vt %s"))
+
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8-unix)
 
