80c19ee8b12ea03147a5571b5f0b956c2f791e15
Björn Ramberg <bjornr@spotify.com>
2021-02-05 10:17:40 +0100

    Remove old keybind

--- init.el
+++ init.el
@@ -232,7 +232,6 @@
     (global-set-key "\C-s" 'swiper)
     (global-set-key (kbd "C-c C-r") 'ivy-resume)
     (global-set-key (kbd "<f6>") 'ivy-resume)
-    (global-set-key (kbd "M-i") 'ivy-imenu-anywhere)
     (global-set-key (kbd "M-x") 'counsel-M-x)
     (global-set-key (kbd "C-x b") 'counsel-switch-buffer)
     (global-set-key (kbd "C-x C-f") 'counsel-find-file)
